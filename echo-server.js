c58b72385a8dbe08a61a9cba9cc67d6759ce9c95
user <user@koalaok.local>
2015-03-27 11:59:26 +0800

    Add a simple echo-server example

---
+++ echo-server.js
@@ -0,0 +1,63 @@
+/**
+ * 启动: node echo-server.js 9000
+ */
+(function() {
+    var listenPort = process.argv[2];
+    var http = require('http');
+    var sockjs = require('sockjs');
+    var sockServer = sockjs.createServer();
+    var httpServer = http.createServer();
+
+    // 连接数
+    var totalConns = 0;
+    sockServer.on('connection', function(socket) {
+        // 如果部署了反向代理，通常需要从 request header 获取真实 ip
+        var ip = socket.headers['x-forwarded-for'];
+
+        totalConns++;
+        log('%s connected', ip);
+
+        socket.on('close', function() {
+            totalConns++;
+            log('%s disconnected', ip);
+        });
+
+        socket.on('data', function(message) {
+            log('%s onData', message);
+            // echo server 啥也不做，只是返回收到的字符
+            socket.write(message);
+        });
+    });
+    sockServer.installHandlers(httpServer, {
+        prefix: '/sock', // URI前缀，当一台服务器部署多个 SockJS 服务时非常有用
+        heartbeat_delay: 25000,
+        disconnect_delay: 5000,
+        log: function(severity, message) {
+            // severity: debug|info|error
+            if (severity === 'error') {
+                log('[SockJS] %s', message);
+            }
+        }
+    });
+
+    if (!listenPort) {
+        console.log('Usage: node echo-server.js port');
+        return;
+    }
+
+    httpServer.listen(listenPort, '0.0.0.0');
+    log('Server started on port %s', listenPort);
+
+    /**
+     * 因为日志都是写到日志文件，所以时间每条日志应该包含几个重要信息：
+     * 1. 时间
+     * 2. 用户ID
+     * 这里只实现了自动包含时间
+     */
+    function log() {
+        var strDate = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+        arguments[0] = '%s ' + arguments[0];
+        [].splice.call(arguments, 1, 0, strDate);
+        console.log.apply(console, arguments);
+    }
+}).call(this);
